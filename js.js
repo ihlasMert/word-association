b4e74debae38ce6d9c8ae7f105a34e3c1e3ae053
İHLASMERT <ihlas1mert@gmail.com>
2022-11-22 18:56:18 +0300

    added array

--- js.js
+++ js.js
@@ -1,2 +1,20 @@
 const scoreDisplay = document.getElementById("score-display");
-const questionDsiplay = document.getElementById("question-display")
+const questionDisplay = document.getElementById("question-display");
+
+const questions = [
+  {
+    quiz: ["value", "estimate", "evaluate"],
+    options: ["jury", "assess"],
+    correct: 2,
+  },
+  {
+    quiz: ["assume", "insight", "weather"],
+    options: ["forecast", "sustainable"],
+    correct: 1,
+  },
+  {
+    quiz: ["fast", "quick", "prompt"],
+    options: ["charity", "rapid"],
+    correct: 2,
+  },
+];
